122dc9a26d72d3274415e83c4ee14d5fea5f96f5
alpi <alpihola@gmail.com>
2016-09-21 16:23:51 -0300

    "helpers"

--- app/src/main/java/com/example/alpi/hellogridview/helpers/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/example/alpi/hellogridview/helpers/SimpleItemTouchHelperCallback.java
@@ -1,9 +1,11 @@
 package com.example.alpi.hellogridview.helpers;
 
+import android.content.ClipData;
 import android.graphics.Canvas;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 /**
  * Created by alpi on 9/21/16.
@@ -72,4 +74,30 @@ public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
         }
     }
 
+    @Override
+    public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+        //cambia solo el item activo
+        if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+            if (viewHolder instanceof ItemTouchHelperViewHolder) {
+                //se notifica al receptor de la vista que este item está siendo movido
+                ItemTouchHelperViewHolder itemViewHolder = (ItemTouchHelperViewHolder) viewHolder;
+                itemViewHolder.onItemSelected();
+            }
+        }
+
+        super.onSelectedChanged(viewHolder, actionState);
+    }
+
+    @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+
+        viewHolder.itemView.setAlpha(ALPHA_FULL);
+
+        if (viewHolder instanceof ItemTouchHelperViewHolder) {
+            //le avisa al receptor de la vista que es tiempo de volver al estado de reposo
+            ItemTouchHelperViewHolder itemViewHolder = (ItemTouchHelperViewHolder) viewHolder;
+            itemViewHolder.onItemClear();
+        }
+    }
 }
